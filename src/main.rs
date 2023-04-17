c8b7632e5efe9b7c386447919755ed33428a279f
Andrei Dodu <andrei.dodu@hotmail.it>
2023-04-17 22:12:27 +0200

    first version

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
+
+fn main() {
+    let mut stream = TcpStream::connect("andre-i.eu:80").unwrap();
+    stream
+        .write("GET http://azenv.net/ HTTP/1.1\r\n".as_bytes())
+        .unwrap();
+    stream.write("Host: azenv.net\r\n".as_bytes()).unwrap();
+    stream
+        .write("User-Agent: Go-http-client/1.1\r\n".as_bytes())
+        .unwrap();
+    stream.write("\r\n".as_bytes()).unwrap();
+    let mut buffer = String::new();
+    stream.read_to_string(&mut buffer).unwrap();
+    println!("{}", buffer);
+    println!(
+        "\r\nyour server is: {}",
+        if buffer.contains("<title>403 Forbidden</title>") {
+            "secure"
+        } else {
+            "usecure"
+        }
+    );
+}
